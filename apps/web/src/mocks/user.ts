2050c5c7c0b244363b0345f13dd25d52bc12c731
Salaaaaas <ssalasleiton@gmail.com>
2026-04-13 22:55:25 -0600

    feat: implementar tipos y mocks para usuarios, posts y comunidades #14

---
+++ apps/web/src/mocks/user.ts
@@ -0,0 +1,19 @@
+import { User } from "../types/user";
+
+export const MOCK_USERS: User[] = [
+    {
+        id: "1",
+        username: "Salas",
+        avatar: "https://i.pravatar.cc/150?u=1"
+    },
+    {
+        id: "2",
+        username: "William",
+        avatar: "https://i.pravatar.cc/150?u=2"
+    },
+    {
+        id: "3",
+        username: "Eli",
+        avatar: "https://i.pravatar.cc/150?u=3"
+    }
+]
---
+++ apps/web/src/types/comment.ts
@@ -0,0 +1,6 @@
+export interface Comment{
+    id: string;
+    body: string;
+    authorId: string;
+    postId: string;
+}
